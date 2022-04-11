abd766d5b3ef403c6c40b4f6cbbee44537278586
shailesh287 <shaileshbind28@gmail.com>
2022-04-11 12:16:58 +0530

    adding function

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import ClassComp from "./Components/ClassComp";
+import FuncComp from "./Components/FuncComp";
 
 const App = () => {
   return (
     <div>
+      <FuncComp />
       <ClassComp />
     </div>
   );
