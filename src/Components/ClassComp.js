453708900d73777a7be0734340790bd6153c7813
shailesh287 <shaileshbind28@gmail.com>
2022-04-11 12:10:30 +0530

    react

---
+++ src/Components/ClassComp.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+
+export class ClassComp extends Component {
+  state = {
+    name: "Shailesh",
+    change: true,
+    count: 0,
+  };
+
+  onChange = () => {
+    this.setState({
+      change: !this.state.change,
+      count: this.state.count + 1,
+    });
+  };
+  render() {
+    return (
+      <>
+        <h1> Class Component </h1>
+
+        <button onClick={this.onChange}>Click-{this.state.count}</button>
+        {this.state.change ? <h2>{this.state.name} </h2> : <h2>New User </h2>}
+      </>
+    );
+  }
+}
+
+export default ClassComp;
